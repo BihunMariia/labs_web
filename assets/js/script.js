f9f646e39172be96338811f9149db46953698a2d
BihunMariia <mariabigun27@gmail.com>
2023-11-05 22:53:09 +0200

    lab4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 import {
     addItemToPage,
-    renderItemsList
+    renderItemsList,
+    editItem,
 } from "./dom_util.js"
 
 const clearButton = document.getElementById("clear_button");
@@ -9,6 +10,34 @@ const searchButton = document.getElementById("search_button");
 const calculateButton = document.getElementById("calculate_button");
 const resultPlace = document.getElementById("result");
 const sortZoos = document.getElementById("sort_button");
+const tab1 = document.getElementById("tab-1");
+const tab2 = document.getElementById("tab-2");
+const formHeader = document.getElementById("form-header");
+const id = document.getElementById("id");
+const nameInput = document.getElementById("name_input");
+const amountOfAnimalsInput = document.getElementById("amount_of_animals_input");
+const visitorsPerYearInput = document.getElementById("visitors_per_year_input");
+
+
+
+
+
+
+const tabLinks = document.querySelectorAll('.tab-link');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabLinks.forEach(tabLink => {
+    tabLink.addEventListener('click', () => {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active');
+        });
+
+        const tabId = tabLink.getAttribute('data-tab');
+        const tabContent = document.getElementById(tabId);
+        tabContent.classList.add('active');
+    });
+});
+
 
 let zoos = [
     { id: 1, name: "Loro Parque", animals: 4000, visitors: 500000 },
@@ -60,4 +89,72 @@ clearButton.addEventListener("click", (event) => {
     event.preventDefault();
     searchInput.value = "";
     renderItemsList(zoos);
+});
+
+
+function validateForm() {
+    if (!document.getElementById('create_zoo').checkValidity()) {
+        alert('Please fill in appropriate data.');
+        return false;
+    }
+    return true;
+}
+
+document.getElementById('create_zoo').addEventListener('submit', function (event) {
+    if (!validateForm()) {
+        return;
+    }
+    event.preventDefault();
+
+    const id = document.getElementById('id').innerHTML;
+    const name = document.getElementById('name_input').value;
+    const animals = parseInt(document.getElementById('amount_of_animals_input').value);
+    const visitors = document.getElementById('visitors_per_year_input').value;
+
+    if (id === "") {
+        const object = { id: zoos.length + 1, name: name, animals: animals, visitors: visitors };
+        addItemToPage(object);
+    } else {
+        const itemId = id.replace("item-", "");
+        const object = { id: itemId, name: name, animals: animals, visitors: visitors };
+        editItem(object);
+    }
+
+    const editButtons = document.getElementsByClassName("edit_button");
+    Array.prototype.forEach.call(editButtons, function (el) {
+        el.addEventListener("click", () => {
+            tab1.classList.remove('active');
+            tab2.classList.add('active');
+            formHeader.textContent = "Edit zoo";
+            id.textContent = el.getAttribute("a");
+            nameInput.value = el.getAttribute("b");
+            amountOfAnimalsInput.value = el.getAttribute("c");
+            visitorsPerYearInput.value = el.getAttribute("d");
+        });
+    });
+
+    const form = document.getElementById('create_zoo');
+    form.reset();
+    tab2.classList.remove('active');
+    tab1.classList.add('active');
+    formHeader.textContent = "Create zoo";
+    id.textContent = "";
+    nameInput.value = "";
+    amountOfAnimalsInput.value = "";
+    visitorsPerYearInput.value = "";
+});
+
+
+
+const editButtons = document.getElementsByClassName("edit_button");
+Array.prototype.forEach.call(editButtons, function (el) {
+    el.addEventListener("click", () => {
+        tab1.classList.remove('active');
+        tab2.classList.add('active');
+        formHeader.textContent = "Edit zoo";
+        id.textContent = el.getAttribute("a");
+        nameInput.value = el.getAttribute("b");
+        amountOfAnimalsInput.value = el.getAttribute("c");
+        visitorsPerYearInput.value = el.getAttribute("d");
+    });
 });
